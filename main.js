df62127ed8cc29dc5d0f1f47d4cd0e8a229b61fd
Sadia Haque <sadiahaque2017@gmail.com>
2026-01-08 01:06:01 +0600

    database connection

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const dbConnection = require("./dababase/dbConnection");
+const app = express();
+const port = 3000;
+require('dotenv').config()
 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+dbConnection()
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
